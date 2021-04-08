84332e70a51ff7760f4a0be0afb337ca6f6f14b6
UpBrainery-Technologies <74796836+UpBrainery-Technologies@users.noreply.github.com>
2021-04-08 10:28:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -26,6 +26,7 @@ firebase.initializeApp(firebaseConfig);
 
   //reference for the folder of the models
   let storageRef = storage.ref(folderPath[1]);
+console.log(storageRef);
   let pattRef = storage.ref();
   let modelRef = storage.ref();
 
@@ -34,7 +35,6 @@ firebase.initializeApp(firebaseConfig);
       res.prefixes.forEach(folderRef => {
         // All the prefixes under listRef.
       folderRef.listAll().then(res =>{
-          console.log(res);
         let id = Math.floor((Math.random() * 100) + 1)
           pattRef.child(res.items[1].fullPath).getDownloadURL().then(url=>{
             let pattUrl = url.split("&");
